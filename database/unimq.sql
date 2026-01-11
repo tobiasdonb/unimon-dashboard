0ea06ec3f1843a78ebf800945b45ce0588093786
okapmna <okapramn092@gmail.com>
2026-01-12 04:13:02 +0800

    add port, device_name to device table

--- database/unimq.sql
+++ database/unimq.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.1deb3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 30, 2025 at 05:09 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:3306
+-- Waktu pembuatan: 11 Jan 2026 pada 20.00
+-- Versi server: 10.11.13-MariaDB-0ubuntu0.24.04.1
+-- Versi PHP: 8.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `device`
+-- Struktur dari tabel `device`
 --
 
 CREATE TABLE `device` (
@@ -32,21 +32,24 @@ CREATE TABLE `device` (
   `broker_url` varchar(500) NOT NULL,
   `mq_pass` varchar(20) DEFAULT NULL,
   `mq_user` varchar(20) DEFAULT NULL,
-  `device_type` varchar(50) NOT NULL
+  `device_type` varchar(50) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `device_name` varchar(50) NOT NULL,
+  `broker_port` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `device`
+-- Dumping data untuk tabel `device`
 --
 
-INSERT INTO `device` (`device_id`, `broker_url`, `mq_pass`, `mq_user`, `device_type`) VALUES
-(0, '', 'pdk2025', 'pdk', 'esp32-inkubator'),
-(0, 'broker.hivemq.com', '', '', 'esp32-inkubator');
+INSERT INTO `device` (`device_id`, `broker_url`, `mq_pass`, `mq_user`, `device_type`, `user_id`, `device_name`, `broker_port`) VALUES
+(14, 'test.mosquitto.org', '', '', 'esp32-inkubator', 11, 'Inkubator Ternak 1', '8080'),
+(19, '1118e8175bef4f45b3d76681ba47bf64.s1.eu.hivemq.cloud', '54321Jan', 'iwasabird', 'esp32-inkubator', 11, 'Inkubator burung', '8884');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -56,32 +59,54 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`user_id`, `user_name`, `password`) VALUES
-(7, 'messi', '$2y$10$7NQnMnIhGj3qWyDa5.Dtp.2iu5Xu5qWv3erRRHKTKqA6QA.MApSs.'),
-(8, 'Novi', '$2y$10$KsGVYuM.K6qzemkkiNAX5eFqlA33Llq4rdhycMJH4p76DeQWsVXtq');
+(11, 'rusdingawi', '$2y$10$mAlEsUu84Iio1NRM8RmRnuc/SWCe1Yy5aNimQ28JKrUKeYTHAmiL.');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `device`
+--
+ALTER TABLE `device`
+  ADD PRIMARY KEY (`device_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `device`
+--
+ALTER TABLE `device`
+  MODIFY `device_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `device`
+--
+ALTER TABLE `device`
+  ADD CONSTRAINT `device_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
